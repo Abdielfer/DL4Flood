a393aa521390ae093aafc17e3a45e1b0d0adc57a
Abdielfer <abdielfer@gmail.com>
2023-05-08 13:24:32 -0400

    Getting ready for first test 1 epoch

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import hydra
+import hydra._internal.instantiate._instantiate2 as instantiate
 from model_set.models import UNetFlood
 from scr import util as U
 from scr import dataLoader as D
 from scr import models_trainer as MT
-# from scr import losses as L
+from scr import util as U
+from scr import losses as L
 from scr.losses import iou_binary,binaryAccuracy, lovasz_hinge 
 from omegaconf import DictConfig, OmegaConf
+from torch.optim import Adam, SGD
 
 class computeStandardizers():
     def __init__(self, cfg:DictConfig) -> None:
@@ -33,45 +36,75 @@ class applyPermanentTransformation():
     def transform(self):
         D.offlineTransformation(self.dataSourcePath,self.transitFolder)
 
+
+class excecuteTrainingVelum():
+    def __init__(self, cfg:DictConfig) -> None:
+
+        trainSetList = cfg['trainingDataList']
+        valSetList = cfg['validationDataList']
+        testSetList = cfg['testingDataList']
+
+        args = {'batch_size': 1, 'num_workers': 4,'drop_last': True}
+        trainDataSet = D.customDataSet(trainSetList)
+        train_DLoader = D.customDataloader(trainDataSet,args)   
+        valDataSet = D.customDataSet(valSetList)
+        val_DLoader = D.customDataloader(valDataSet,args)
+        testDataSet = D.customDataSet(testSetList)
+        test_DLoader = D.createTransformation(testDataSet,args)
+        
+        model = UNetFlood(1,1)
+        loss_fn = L.lovasz_hinge
+        optimizer = Adam(model.parameters(), lr = 0.001) #SGD(model.parameters(), lr=0.001, momentum=0.9)##  
+        
+        trainer = MT.models_trainer(model,loss_fn,optimizer, iou_binary)
+        trainer.set_loaders(train_DLoader,val_DLoader,test_DLoader)
+        
+        trainLosses, valLosses, testLosses = trainer.train(2)
+        return model, [trainLosses, valLosses, testLosses]
+                    
+
 class excecuteTraining():
     def __init__(self, cfg:DictConfig) -> None:
-        args = cfg['parameters']
-        trainDataSet = D.customDataSet(cfg['trainingDataList'])
-        train_DLoader = D.customDataloader(trainDataSet,args['dataLoaderArgs'])   
-        valDataSet = D.customDataSet(cfg['validationDataList'])
-        val_DLoader = D.customDataloader(valDataSet,args['dataLoaderArgs'])
-        testDataSet = D.customDataSet(cfg['testingDataList'])
-        test_DLoader = D.createTransformation(testDataSet,args['dataLoaderArgs'])
-        model = args['model']
-        loss_fn = args['loss_fn']
-        optimizer = args['optimizer']
-        optimizer = optimizer(model.parameters(),args['optimizerParams'])
-        metric = args['metric_fn']
-        trainer = MT.models_trainer(model,loss_fn,optimizer, metric)
-        trainer.set_loaders(train_DLoader,val_DLoader,test_DLoader)
-        trainLosses, valLosses, testLosses = trainer.train(args['epochs'])
-        return model, metric, [trainLosses, valLosses, testLosses]
+        model = OmegaConf.create(cfg.parameters.model)
+        # print(args)
+        # trainDataSet = D.customDataSet(cfg['trainingDataList'])
+        # train_DLoader = D.customDataloader(trainDataSet,args['dataLoaderArgs'])   
+        # valDataSet = D.customDataSet(cfg['validationDataList'])
+        # val_DLoader = D.customDataloader(valDataSet,args['dataLoaderArgs'])
+        # testDataSet = D.customDataSet(cfg['testingDataList'])
+        # test_DLoader = D.createTransformation(testDataSet,args['dataLoaderArgs'])
+        model = instantiate(model)
+        # loss_fn = instantiate(cfg.model)
+        # optimizer = args['optimizer']
+        # optimizer = optimizer(model.parameters(),args['optimizerParams'])
+        # metric = instantiate(cfg.metric_fn)
+        # trainer = MT.models_trainer(model,loss_fn,optimizer, metric)
+        # trainer.set_loaders(train_DLoader,val_DLoader,test_DLoader)
+        # trainLosses, valLosses, testLosses = trainer.train(args['epochs'])
+        # return model, metric, [trainLosses, valLosses, testLosses]
+        print("model >>","\n", model )
     
 @hydra.main(version_base=None, config_path=f"config", config_name="configPC.yaml")
 def main(cfg: DictConfig):
 
-    ## Spliting Trn-Val
-    trnList, valList = U.splitPerRegion(cfg['rawDataList'])
-    U.createCSVFromList(cfg['trainingDataList'], trnList)
-    U.createCSVFromList(cfg['validationDataList'],valList)
+    # ## Spliting Trn-Val
+    # trnList, valList = U.splitPerRegion(cfg['rawDataList'])
+    # U.createCSVFromList(cfg['trainingDataList'], trnList)
+    # U.createCSVFromList(cfg['validationDataList'],valList)
 
     # ### Performe offline transformations
-    transformer = applyPermanentTransformation(cfg)
-    transformer.transform()
+    # transformer = applyPermanentTransformation(cfg)
+    # transformer.transform()
 
     ### Compute standardizers
     # MinMaxMeanSTD = computeStandardizers(cfg)
     # print(MinMaxMeanSTD)
     
     ## Training cycle
+    model,losses = excecuteTrainingVelum(cfg)
     # model, metric, losses = excecuteTraining(cfg)
-    # name = model.name
-    # U.saveModel(model, name)
+    name = model.name
+    U.saveModel(model, name)
 
 
 if __name__ == "__main__":
